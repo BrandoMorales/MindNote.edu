9358e59bec5c7eb6ca461fbd80c966c2488a7c5e
BrandoMorales <felipemoralesherrera888@gmail.com>
2025-11-05 13:47:41 -0500

    vite

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/MindNote.edu">
+    <BrowserRouter basename="/MindNote.edu/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
