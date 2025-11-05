99213ea27b08092c2aaa79be6c80c25add967edb
BrandoMorales <felipemoralesherrera888@gmail.com>
2025-11-05 14:07:59 -0500

    dd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Configuración para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/MindNote.edu/', // 👈 nombre exacto del repositorio
+  base: '/MindNote.edu/', // 👈 debe coincidir EXACTAMENTE con el nombre del repo
 })
