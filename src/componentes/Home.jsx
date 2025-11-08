f3fdfa71a61f6ee642edb23165ad951eef52a220
BrandoMorales <felipemoralesherrera888@gmail.com>
2025-11-08 17:27:15 -0500

    Limpieza de archivos grandes

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -11,11 +11,6 @@ const Home = () => {
 
   return (
     <div className="home">
-      <video autoPlay muted loop className="background-video">
-        <source src="src/assets/videoplayback.mp4" type="video/mp4" />
-        Tu navegador no soporta videos HTML5.
-      </video>
-
       <div className="home-box animate-fade-in">
         <h1>Bienvenido a MindNote.edu</h1>
         <p>Tu espacio para organizar notas, motivaciones y emociones.</p>
